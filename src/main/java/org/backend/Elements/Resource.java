d6b58fe9b27da4f58b5d48a878f73985254b9b8b
Paul RADIC <paul.radic@epita.fr>
2021-04-18 16:24:31 +0200

    [Resource] adding resource entity and formdata

--- src/main/java/org/backend/Elements/Resource.java
+++ src/main/java/org/backend/Elements/Resource.java
@@ -1,4 +1,46 @@
-package org.backend.Elements.Components.Resource;
+package org.backend.Elements;
 
-public class Resource {
+import org.backend.Elements.Enum.ResourceType;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Resource extends WithId{
+    private String name;
+    private ResourceType type;
+    private String path;
+    public Resource() {
+    }
+
+    public Resource(String name, ResourceType type) {
+        this.name = name;
+        this.type = type;
+        this.path = type.name() + "/" + name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ResourceType getType() {
+        return type;
+    }
+
+    public void setType(ResourceType type) {
+        this.type = type;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
--- src/main/java/org/backend/FormData/FormData.java
+++ src/main/java/org/backend/FormData/FormData.java
@@ -1,2 +1,20 @@
-package org.backend.FormData;public class FormData {
+package org.backend.FormData;
+
+
+import org.jboss.resteasy.reactive.PartType;
+import org.jboss.resteasy.reactive.RestForm;
+import org.jboss.resteasy.reactive.multipart.FileUpload;
+
+import javax.ws.rs.core.MediaType;
+import java.io.File;
+
+public class FormData {
+    @RestForm
+    @PartType(MediaType.TEXT_PLAIN)
+    public String name;
+    @RestForm
+    @PartType(MediaType.TEXT_PLAIN)
+    public String type;
+    @RestForm("file")
+    public File file;
 }
